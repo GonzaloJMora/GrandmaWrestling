6d730c6714a29afc74f30e7bb40a6fd934c2d0d3
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2023-03-12 11:50:59 -0400

    Thwomp

---
+++ Assets/ChaosSystem/Thwomps/ThwompMovement.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ThwompMovement : MonoBehaviour
+{
+    private enum ThwompStates { Moving, Shaking, Attacking, Recovering, Resetting};
+
+    [SerializeField] private float speed;
+    [SerializeField] private float maxHeight;
+    [SerializeField] private float shakeTime;
+    [SerializeField] private float fallSpeed;
+    [SerializeField] private float recoveryTime;
+    private float radius;
+
+    [SerializeField] private Vector3 targetPos;
+
+    private GameObject[] players;
+
+    private ThwompStates state;
+    private float currTime = 0f;
+    private float currShakeTime = 0f;
+    private float sign = 1f;
+    private float minFallHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minFallHeight = gameObject.transform.localScale.y / 2;        
+        NewTargetPos();
+        state = ThwompStates.Moving;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(state);
+        if (state == ThwompStates.Moving)
+        {
+            float dist = Mathf.Abs(Vector3.Distance(targetPos, transform.position));
+            if(dist < 0.1f)
+            {
+                state = ThwompStates.Shaking;
+                //NewTargetPos();
+            }
+            transform.position += Vector3.Normalize(targetPos - transform.position) * speed * Time.deltaTime;
+        }
+        else if(state == ThwompStates.Shaking)
+        {
+            currTime += Time.deltaTime;
+            currShakeTime += Time.deltaTime;
+            if (currTime > 0.05f)
+            {
+                float degree = Random.Range(0f, 15f) * sign;
+                sign *= -1f;
+                var nine = Quaternion.AngleAxis(degree, Vector3.forward);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, nine, 90f);//nine * transform.rotation
+                currTime = 0f;
+            }
+            if(currShakeTime > shakeTime)
+            {
+                //reset rotation
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(0, Vector3.forward), 90f);
+                state = ThwompStates.Attacking;
+                currTime = 0f;
+                currShakeTime = 0f;
+            }
+        }
+        else if(state == ThwompStates.Attacking)
+        {
+            //float dist = Mathf.Abs(minFallHeight - transform.position.y);
+            if(transform.position.y < minFallHeight)
+            {
+                state = ThwompStates.Recovering;
+            }
+            transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+        }
+        else if(state == ThwompStates.Recovering)
+        {
+            currTime += Time.deltaTime;
+            if(currTime > recoveryTime)
+            {
+                currTime = 0f;
+                state = ThwompStates.Resetting;
+            }
+        }
+        else if(state == ThwompStates.Resetting)
+        {
+            //float dist = Mathf.Abs(maxHeight - transform.position.y);
+            if (transform.position.y > maxHeight)
+            {
+                state = ThwompStates.Moving;
+                NewTargetPos();
+            }
+            transform.position += Vector3.up * fallSpeed * Time.deltaTime;
+        }
+        
+
+
+    }
+
+    private void NewTargetPos()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log(players.Length);  
+        int targetIndex = Random.Range(0, players.Length);
+        Vector3 t = players[targetIndex].transform.position;
+        targetPos = new Vector3(t.x, maxHeight, t.z);
+    }
+
+    public void SetRadius(float rad)
+    {
+        radius = rad;
+    }
+
+    public void SetHeight(float height)
+    {
+        maxHeight = height;
+    }
+
+
+}
---
+++ Assets/ChaosSystem/Thwomps/Thwomps.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thwomps : Chaos
+{
+
+    [SerializeField] private Vector2Int minMaxSpawnAnoumt;
+    [SerializeField] private float spawnDelay;
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private float spawnRadius = 1.98f;
+    [SerializeField] private float maxHeight = 10f;
+    private GameObject par;
+    public override void Stop()
+    {
+        Destroy(par);
+        //throw new System.NotImplementedException();
+    }
+
+    public override void Trigger()
+    {
+        par = new GameObject();
+        
+        int amount = Random.Range(minMaxSpawnAnoumt.x, minMaxSpawnAnoumt.y);
+
+        StartCoroutine(SpawnThwomps(amount, par));
+
+        //throw new System.NotImplementedException();
+    }
+
+    IEnumerator SpawnThwomps(int spawnAmount, GameObject p)
+    {
+        int i = 0;
+        while (i < spawnAmount)
+        {
+            GameObject t = Instantiate(prefab, par.transform, false);
+
+            Vector3 spawnPos = Random.insideUnitCircle * spawnRadius;
+            t.transform.position = spawnPos + new Vector3(0f, maxHeight, 0f);
+            ThwompMovement tm = t.GetComponent<ThwompMovement>();
+            tm.SetRadius(spawnRadius);
+            tm.SetHeight(maxHeight);
+
+
+            i += 1;
+            //Debug.Log("i: " + i);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+
+}
